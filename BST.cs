eb70175ba8674ccb5c4099f6d841fbf4839fffb1
Nicolas Timm <nntimm1@gmail.com>
2020-01-31 08:08:04 -0600

    project Start

---
+++ BST.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BST
+    {
+        // ------------------------------- Member Valiable ------------------------------------
+       
+        public Node root;
+
+
+
+
+
+        //  ------------------------------ Constructor -----------------------------------------
+        public BST()
+        {
+            
+        }
+                     
+
+        // -------------------------------- Member method --------------------------------------
+
+        public void Add()
+        {
+            if (root == null)
+            { root = data; }
+            else if (root >= data)
+            { Node rightChild = data; }
+            else
+            { Node leftChild == data; }
+        }
+
+
+
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        // ------------------------------- Member Valiable ------------------------------------
+        int data;
+        Node leftChild;
+        Node rightchild;
+
+
+
+
+
+        //  ------------------------------ Constructor -----------------------------------------
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+
+
+
+
+
+
+        // -------------------------------- Member method --------------------------------------
+
+
+
+
+
+    }
+}
